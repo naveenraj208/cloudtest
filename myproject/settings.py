53eec49e3e8d24312250b754cff8a1589f836dd3
B U NAVEEN RAJ <131900909+naveenraj208@users.noreply.github.com>
2025-05-14 20:21:12 +0530

    PLS

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'et_db',
+        'USER': 'ET_user',
+        'PASSWORD': 'NAVEEN',
+        'HOST': 'finalcloud-459801:asia-south1:cloudtestdb',
+        'PORT': '5432',
     }
 }
 
