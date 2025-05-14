167023d8b11cff8e16f12d70527c4c9714c595d1
B U NAVEEN RAJ <131900909+naveenraj208@users.noreply.github.com>
2025-05-14 20:36:23 +0530

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,16 +74,6 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'et_db',
-        'USER': 'ET_user',
-        'PASSWORD': 'NAVEEN',
-        'HOST': 'finalcloud-459801:asia-south1:cloudtestdb',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
